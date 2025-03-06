1e1bbc422db9f7b7b868a81fc8ee85b878407be7
minagishl <mvxd568.co@icloud.com>
2025-03-06 18:58:34 +0900

    refactor: remove unnecessary class names

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -101,9 +101,7 @@ export default function Home() {
                 stroke-linejoin="round"
                 d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418"
               />
-              <h5 class="ml-3 text-2xl text-sm font-semibold text-white">
-                LT大会とは
-              </h5>
+              <h5 class="ml-3 text-sm font-semibold text-white">LT大会とは</h5>
             </div>
             <p class="mb-4 block whitespace-pre-wrap font-light leading-normal text-slate-200">
               {text}
